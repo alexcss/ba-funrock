0e371fcdf4e5d2ab10971a7353fa837adc3768fa
AlexCSS <kalyuzhnyi@gmail.com>
2017-03-31 22:21:00 +0300

    add screenshots

--- page-templates/page-our-games.php
+++ page-templates/page-our-games.php
@@ -23,7 +23,7 @@ get_header(); ?>
 				<div class="fr-ipad-slider">
 					<?php $slider = get_field('slider'); ?>
 					<?php foreach ($slider as $img): ?>
-						<img src="<?php echo $img['sizes']['fr-ipad-slider']  ?>" alt="<?php echo $img['sizes']['fr-ipad-slider']  ?>">
+						<img src="<?php echo $img['sizes']['fr-ipad-slider']  ?>" alt="<?php echo $img['title']  ?>">
 					<?php endforeach ?>
 				</div>
 				<!-- /.fr-ipad-slider -->
@@ -34,6 +34,28 @@ get_header(); ?>
 
 			</div>
 			<!-- END column medium-6 -->
+		</div>
+		<!-- /.row column -->
+	</section>
+
+	<section>
+		<div class="row column">
+			<h3><?php the_field('screenshots_title'); ?></h3>
+
+			<?php $screenshots = get_field('screenshots'); ?>
+			<!-- BEGIN row  -->
+			<div class="row">
+				<?php foreach ($screenshots as $img): ?>
+					<!-- BEGIN column medium-6 large-4  -->
+					<div class="column medium-6 large-4">
+						<a href="<?php echo $img['url']  ?>" class="ba-screenshot" data-button="<?php _e('View Gallery') ?>">
+							<img src="<?php echo $img['sizes']['fr-screenshot']  ?>" alt="<?php echo $img['title']  ?>">
+						</a>
+					</div>
+					<!-- END column medium-6 large-4 -->
+				<?php endforeach ?>
+			</div>
+			<!-- END row -->
 		</div>
 		<!-- /.row column -->
 	</section>
